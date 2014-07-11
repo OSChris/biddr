1d189bdea17325d49f59090d01a2dc14c4fdd6b6
Chris Yammine <chris.yammine@hotmail.com>
2014-07-11 10:28:02 -0700

    Add bids to auctions Add user references to auctions and bids Add navbar and links to devise actions

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,7 +1,49 @@
 class AuctionsController < ApplicationController
 
+  before_action :find_auction, except: [:index, :new, :create]
+
   def index
+    @auctions = Auction.all
   end
 
+  def show
+  end
+
+  def new
+    @auction = Auction.new
+  end
+
+  def create
+    @auction = Auction.new(auction_params)
+    @auction.user = current_user
+    if @auction.save
+      redirect_to @auction, notice: "Auction created!"
+    else
+      render :new
+      flash.now[:alert] = "Nope."
+    end
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @auction.destroy
+    redirect_to auctions_path, notice: "Auction deleted!"
+  end
+
+  private
+
+  def auction_params
+    params.require(:auction).permit(:title, :details, :end_date, :reserve)
+  end
+
+  def find_auction
+    @auction = Auction.find params[:id]
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :bids
+
   devise_for :users
   resources :auctions
 
